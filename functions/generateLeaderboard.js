bfc3e1096e6dada2e8d07c87e7e222f74bc2fa93
Hemu21 <rajva5727@gmail.com>
2024-07-30 01:33:55 +0530

    updated

--- functions/generateLeaderboard.js
+++ functions/generateLeaderboard.js
@@ -1,226 +1,210 @@
 var fs = require("fs");
 var axios = require("axios");
-const { type } = require("os");
 require("dotenv").config();
-const logStream = fs.createWriteStream("debug.log", { flags: "a" });
+
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 let leaderboard = {};
 
 async function generateLeaderboard() {
-  let projects = await axios.get(
-    `https://opensheet.elk.sh/1JiqHjGyf43NNkou4PBe7WT4KEyueuFJct2p322nNMNw/JSON`
-  );
-  leaderboard = {};
-  projects = projects.data;
-  let identifyingLabel = "gssoc23";
-  let labels = [
-    {
-      label: "level1",
-      points: 10,
-    },
-    {
-      label: "level2",
-      points: 25,
-    },
-    {
-      label: "level3",
-      points: 45,
-    },
-  ];
-  let dateRanges;
-  for (let m = 0; m < projects.length; m++) {
-    let projectLink = projects[m].project_link;
-    projects[m].project_link =
-      projects[m].project_link.split("/")[3] +
-      "/" +
-      (projects[m].project_link.split("/")[4]
-        ? projects[m].project_link.split("/")[4]
-        : "");
-    if (projects[m].project_link == "GSSoC24/Postman-Challenge") {
-      dateRanges = [
-        { startDate: "2024-07-17", endDate: "2024-07-28" },
-        { startDate: "2024-07-29", endDate: "2024-07-31" },
-      ];
-    } else {
-      dateRanges = [
-        { start: "2024-05-10", end: "2024-05-31" },
-        { start: "2024-06-01", end: "2024-06-30" },
-        { start: "2024-07-01", end: "2024-07-31" },
-      ];
+  try {
+    let response = await axios.get(
+      "https://opensheet.elk.sh/1JiqHjGyf43NNkou4PBe7WT4KEyueuFJct2p322nNMNw/JSON"
+    );
+    let projects = response.data;
+
+    leaderboard = {};
+    let labels = [
+      { label: "level1", points: 10 },
+      { label: "level2", points: 25 },
+      { label: "level3", points: 45 },
+    ];
+
+    for (let m = 0; m < projects.length; m++) {
+      let projectLink = projects[m].project_link
+        .split("/")
+        .slice(3, 5)
+        .join("/");
+
+      let dateRanges =
+        projectLink === "GSSoC24/Postman-Challenge"
+          ? [
+              { startDate: "2024-07-23", endDate: "2024-07-29" },
+              { startDate: "2024-07-30", endDate: "2024-07-31" },
+            ]
+          : [
+              { start: "2024-05-10", end: "2024-06-10" },
+              { start: "2024-06-11", end: "2024-07-10" },
+              { start: "2024-07-11", end: "2024-07-31" },
+            ];
+
+      await processDateRange(projectLink, "2024-05-10", "2024-07-31", labels);
+
+      console.log(`Completed ${m + 1} of ${projects.length}`);
+      await timer(10000);
     }
 
-    for (let dateRange of dateRanges) {
-      await axios
-        .get(
-          `https://api.github.com/search/issues?q=repo:${projects[m].project_link}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc,level1,level2,level3+is:merged+closed:${dateRange.start}..${dateRange.end}&per_page=100`,
-          {
-            headers: {
-              Authorization: "token " + process.env.GIT_TOKEN,
-            },
-          }
-        )
-        .then(async function (response) {
-          if (response.data.items && response.data.items.length > 0) {
-            let prs = response.data.items;
-            //console.log(prs);
-            for (let i = 0; i < prs.length; i++) {
-              for (let j = 0; j < prs[i].labels.length; j++) {
-                if (!leaderboard[prs[i].user.id]) {
-                  leaderboard[prs[i].user.id] = {
-                    avatar_url: prs[i].user.avatar_url,
-                    login: prs[i].user.login,
-                    url: prs[i].user.html_url,
-                    score: 0,
-                    postManTag: false,
-                    pr_urls: [],
-                  };
-                }
-                if (
-                  prs[i].labels[j].name.toLowerCase() === "postman" &&
-                  leaderboard[prs[i].user.id].postManTag == false
-                ) {
-                  leaderboard[prs[i].user.id].postManTag = true;
-                  leaderboard[prs[i].user.id].score += 500;
-                  logStream.write(
-                    `user: ${prs[i].user.login} pr: ${prs[i].html_url} label: ${
-                      prs[i].labels[j].name
-                    } points: ${leaderboard[prs[i].user.id].score} \n`
-                  );
-                }
-                if (
-                  leaderboard[prs[i].user.id].pr_urls.indexOf(
-                    prs[i].html_url
-                  ) == -1
-                ) {
-                  leaderboard[prs[i].user.id].pr_urls.push(prs[i].html_url);
-                }
-                let obj = labels.find((o) => o.label === prs[i].labels[j].name);
-                if (obj) {
-                  leaderboard[prs[i].user.id].score += obj.points;
-                  logStream.write(
-                    `user: ${prs[i].user.login} pr: ${prs[i].html_url} label: ${
-                      prs[i].labels[j].name
-                    } points: ${leaderboard[prs[i].user.id].score} \n`
-                  );
-                }
-              }
-            }
-            if (response.data.total_count > 200) {
-              //calculate number of pages
-              let pages = Math.ceil(response.data.total_count / 200);
-              console.log("========");
-              console.log("No. of pages: " + pages);
-              console.log(
-                `https://api.github.com/search/issues?q=repo:${projects[m].project_link}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc,level1,level2,level3+is:merged+closed:${dateRange.start}..${dateRange.end}&per_page=100`
-              );
-              console.log("========");
-              for (let i = 2; i <= pages; i++) {
-                console.log("Page: " + i);
-
-                let paginated = await axios
-                  .get(
-                    `https://api.github.com/search/issues?q=repo:${projects[m].project_link}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc,level1,level2,level3+is:merged+closed:${dateRange.start}..${dateRange.end}&per_page=100&page=${i}`,
-                    {
-                      headers: {
-                        Authorization: "token " + process.env.GIT_TOKEN,
-                      },
-                    }
-                  )
-                  .then(async function (response) {
-                    console.log("*****" + response.data.items.length);
-                    if (response.data.items && response.data.items.length > 0) {
-                      let prs = response.data.items;
-                      for (let i = 0; i < prs.length; i++) {
-                        for (let j = 0; j < prs[i].labels.length; j++) {
-                          if (!leaderboard[prs[i].user.id]) {
-                            leaderboard[prs[i].user.id] = {
-                              avatar_url: prs[i].user.avatar_url,
-                              login: prs[i].user.login,
-                              url: prs[i].user.html_url,
-                              score: 0,
-                              pr_urls: [],
-                            };
-                          }
-                          if (
-                            prs[i].labels[j].name.toLowerCase() === "postman" &&
-                            leaderboard[prs[i].user.id].postManTag == false
-                          ) {
-                            leaderboard[prs[i].user.id].postManTag = true;
-                            leaderboard[prs[i].user.id].score += 500;
-                            logStream.write(
-                              `user: ${prs[i].user.login} pr: ${
-                                prs[i].html_url
-                              } label: ${prs[i].labels[j].name} points: ${
-                                leaderboard[prs[i].user.id].score
-                              } \n`
-                            );
-                          }
-                          if (
-                            leaderboard[prs[i].user.id].pr_urls.indexOf(
-                              prs[i].html_url
-                            ) == -1
-                          ) {
-                            leaderboard[prs[i].user.id].pr_urls.push(
-                              prs[i].html_url
-                            );
-                          }
-                          let obj = labels.find(
-                            (o) => o.label == prs[i].labels[j].name
-                          );
-                          if (obj) {
-                            leaderboard[prs[i].user.id].score += obj.points;
-                            logStream.write(
-                              `user: ${prs[i].user.login} pr: ${
-                                prs[i].html_url
-                              } label: ${prs[i].labels[j].name} points: ${
-                                leaderboard[prs[i].user.id].score
-                              } \n`
-                            );
-                          }
-                        }
-                      }
-                    }
-                    console.log("Completed page: " + i);
-                  });
-                await timer(10000);
-              }
-            }
-          }
-        })
-        .catch(function (err) {
-          console.log(
-            "Not found for this project link ",
-            projects[m].project_link
-          );
-        });
-    }
-    console.log("Completed " + (m + 1) + " of " + projects.length);
-    await timer(10000);
+    finalizeLeaderboard();
+  } catch (error) {
+    console.error("Error generating leaderboard:", error);
   }
-  // wait for all the prs to be fetched
-  console.log("Leaderboard generated");
-  //sort the leaderboard by score
-  let leaderboardArray = Object.keys(leaderboard).map(
-    (key) => leaderboard[key]
+}
+
+async function processDateRange(projectLink, startDate, endDate, labels) {
+  try {
+    let totalPRs = await fetchTotalPRs(projectLink, "2024-05-10", "2024-07-31");
+    console.log("total", totalPRs, projectLink);
+    if (totalPRs > 1000) {
+      let splitDateRanges = splitDateRange(startDate, endDate);
+      for (let splitDateRange of splitDateRanges) {
+        await fetchPRsForDateRange(
+          projectLink,
+          splitDateRange.start,
+          splitDateRange.end,
+          labels
+        );
+      }
+    } else {
+      if (totalPRs > 0) {
+        await fetchPRsForDateRange(projectLink, startDate, endDate, labels);
+      }
+    }
+  } catch (error) {
+    console.error("Error processing date range:", error);
+  }
+}
+
+async function fetchTotalPRs(projectLink, startDate, endDate) {
+  try {
+    let response = await axios.get(
+      `https://api.github.com/search/issues?q=repo:${projectLink}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc+is:merged+closed:${startDate}..${endDate}&per_page=100`,
+      {
+        headers: {
+          Authorization: "token " + process.env.GIT_TOKEN,
+        },
+      }
+    );
+    return response.data.total_count;
+  } catch (error) {
+    console.error("Error fetching total PRs:", error);
+    return 0;
+  }
+}
+
+async function fetchPRsForDateRange(projectLink, startDate, endDate, labels) {
+  try {
+    let response = await axios.get(
+      `https://api.github.com/search/issues?q=repo:${projectLink}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc+is:merged+closed:${startDate}..${endDate}&per_page=100`,
+      {
+        headers: {
+          Authorization: "token " + process.env.GIT_TOKEN,
+        },
+      }
+    );
+    let totalPRs = response.data.total_count;
+    console.log(totalPRs);
+    let pages = Math.ceil(totalPRs / 100);
+
+    for (let i = 1; i <= pages; i++) {
+      await processPage(projectLink, startDate, endDate, i, labels);
+      await timer(10000);
+    }
+  } catch (error) {
+    console.error("Error fetching PRs for date range:", error);
+  }
+}
+
+async function processPage(projectLink, startDate, endDate, page, labels) {
+  try {
+    let response = await axios.get(
+      `https://api.github.com/search/issues?q=repo:${projectLink}+is:pr+label:gssoc24,GSSoC24,GSSOC'24,gssoc+is:merged+closed:${startDate}..${endDate}&per_page=100&page=${page}`,
+      {
+        headers: {
+          Authorization: "token " + process.env.GIT_TOKEN,
+        },
+      }
+    );
+    console.log("items", response.data.items.length);
+    if (response.data.items && response.data.items.length > 0) {
+      response.data.items.forEach((pr) => {
+        processPR(pr, labels);
+      });
+    }
+  } catch (error) {
+    console.error("Error processing page:", error);
+  }
+}
+
+function processPR(pr, labels) {
+  pr.labels.forEach((label) => {
+    let userId = pr.user.id;
+    if (!leaderboard[userId]) {
+      leaderboard[userId] = {
+        avatar_url: pr.user.avatar_url,
+        login: pr.user.login,
+        url: pr.user.html_url,
+        score: 0,
+        postManTag: false,
+        pr_urls: [],
+      };
+    }
+    if (
+      label.name.toLowerCase() === "postman" &&
+      !leaderboard[userId].postManTag
+    ) {
+      leaderboard[userId].postManTag = true;
+      leaderboard[userId].score += 500;
+    }
+    if (!leaderboard[userId].pr_urls.includes(pr.html_url)) {
+      leaderboard[userId].pr_urls.push(pr.html_url);
+      let labelObj = labels.find((l) => l.label === label.name);
+      if (labelObj) {
+        leaderboard[userId].score += labelObj.points;
+      }
+    }
+  });
+}
+
+function splitDateRange(start, end) {
+  let startDate = new Date(start);
+  let endDate = new Date(end);
+
+  // Calculate the 75% point in milliseconds
+  let duration = endDate.getTime() - startDate.getTime();
+  let seventyFivePercentDate = new Date(startDate.getTime() + duration * 0.75);
+
+  return [
+    { start: start, end: seventyFivePercentDate.toISOString().split("T")[0] },
+    { start: seventyFivePercentDate.toISOString().split("T")[0], end: end },
+  ];
+  // let startDate = new Date(start);
+  // let endDate = new Date(end);
+  // let midDate = new Date((startDate.getTime() + endDate.getTime()) / 2);
+
+  // return [
+  //   { start: start, end: midDate.toISOString().split("T")[0] },
+  //   { start: midDate.toISOString().split("T")[0], end: end },
+  // ];
+}
+
+function finalizeLeaderboard() {
+  let leaderboardArray = Object.values(leaderboard).sort(
+    (a, b) => b.score - a.score
   );
-  leaderboardArray.sort((a, b) => b.score - a.score);
   let json = {
     leaderboard: leaderboardArray,
     success: true,
-    updatedAt: +new Date(),
+    updatedAt: Date.now(),
     generated: true,
     updatedTimestring:
       new Date().toLocaleString() +
       " No New PRs merged after 31st July 11:59p.m are counted",
   };
-  fs.truncate("leaderboard.json", 0, function () {
-    console.log("done");
-  });
+
   fs.writeFile(
     "leaderboard.json",
-    JSON.stringify(json),
+    JSON.stringify(json, null, 2),
     "utf8",
-    function (err) {
+    (err) => {
       if (err) throw err;
       console.log("leaderboard.json was updated");
     }
