8b335dad573712b81c6c6a3c9a9f69700ca80a55
Hemu21 <rajva5727@gmail.com>
2024-10-08 12:53:52 +0530

    update

--- functions/generateLeaderboard.js
+++ functions/generateLeaderboard.js
@@ -131,6 +131,9 @@ async function generateLeaderboard() {
     { label: "level 1", points: 10 },
     { label: "level 2", points: 25 },
     { label: "level 3", points: 45 },
+    { label: "level-1", points: 10 },
+    { label: "level-2", points: 25 },
+    { label: "level-3", points: 45 },
   ];
 
   for (let m = 0; m < projects.length; m++) {
